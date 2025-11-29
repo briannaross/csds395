d31fc812276a3a9a1c0c5fcadbbfe3fac9527738
lalithyagangula <lxg378@case.edu>
2025-11-28 21:22:55 -0500

    Update index.js
    added meal plan connection

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { MealPlanProvider } from "./context/MealPlanContext";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+);
+
+root.render(
+  <MealPlanProvider>
+    <App />
+  </MealPlanProvider>
 );
