5ecd739a2aba3635ae545e7ace66ace143ccc284
lalithyagangula <lxg378@case.edu>
2025-11-28 21:22:28 -0500

    Update App.js
    added meal plan page info

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Login from "./login/Login";
 import Dashboard from "./login/Dashboard";
 import Transactions from "./login/Transactions";
 import { BudgetProvider } from "./context/BudgetContext";
+import MealPlanPage from "./login/MealPlanPage";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/transactions" element={<Transactions />} />
+          <Route path="/meal-plan" element={<MealPlanPage />} />
         </Routes>
       </Router>
     </BudgetProvider>
