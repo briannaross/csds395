a392d8e6df911ae6b50974ce15cde675035769ad
lakshmikunjan <lrk53@case.edu>
2025-12-02 00:26:01 -0500

    Add route for CaseCash component
    Added a new route for the CaseCash component.

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Dashboard from "./login/Dashboard";
 import Transactions from "./login/Transactions";
 import { BudgetProvider } from "./context/BudgetContext";
 import MealPlanPage from "./login/MealPlanPage";
+import CaseCash from "./login/CaseCash";   
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/transactions" element={<Transactions />} />
           <Route path="/meal-plan" element={<MealPlanPage />} />
+          <Route path="/casecash" element={<CaseCash />} /> {/*  ADDED THIS */}
         </Routes>
       </Router>
     </BudgetProvider>
